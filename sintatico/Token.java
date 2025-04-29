c3b32360f373e051464568816aabd99c9c9945d7
danilo <dandavid.ddm@gmail.com>
2025-04-29 20:50:45 -0300

    Adicionando analisador sintatico

---
+++ sintatico/Token.java
@@ -0,0 +1,15 @@
+public class Token {
+	
+	public String tipo;
+	public String lexema;
+	
+	public Token(String tipo, String lexema) {
+		this.lexema = lexema;
+		this.tipo = tipo;
+	}
+	
+	@Override
+	public String toString() {
+		return "<" + this.tipo + ", " + this.lexema + ">";
+	}
+}
